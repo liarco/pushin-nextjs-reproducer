6e0f8498564a516199acda629eb4a3c041cfb444
Marco Lipparini <developer@liarco.net>
2022-05-13 12:32:57 +0200

    Implementing basic demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { PushIn } from 'pushin'
+import { useEffect, useRef } from 'react'
 
 const Home: NextPage = () => {
+  const pushInContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const pushIn = new PushIn(pushInContainer.current!);
+    pushIn.start();
+
+    return () => pushIn.destroy();
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +23,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="pushin" ref={pushInContainer}>
+          <div className="pushin-scene">
+            <div className="pushin-layer">This is the first layer you'll see.</div>
+            <div className="pushin-layer">
+              This is a second layer, which will be positioned behind the first one.
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
